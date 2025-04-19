87346ebc6370ea9b1e66842ad1667117461c96e4
JastinXyz <jastinlinggar@gmail.com>
2025-04-19 20:56:47 +0700

    adding ssr.noExternal for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  ssr: {
+    noExternal: ['react-easy-crop', 'tslib']
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
