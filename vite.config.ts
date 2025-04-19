0e2aec95f2a23b4d7c1b5e0a75b7faca59830d2e
JastinXyz <jastinlinggar@gmail.com>
2025-04-19 19:40:46 +0700

    removing vite plugin import

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import createImportPlugin from 'vite-plugin-import';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), createImportPlugin([
-    {
-      libraryName: '@mui/icons-material',
-      'style': true,   // or 'css'
-    }
-  ])],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
