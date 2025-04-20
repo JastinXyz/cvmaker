976e0ff8d15d04ce277d181bc369587c84003e89
JastinXyz <jastinlinggar@gmail.com>
2025-04-20 16:46:19 +0700

    change footer text

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="mt-4 text-xs text-center">
-            <p>This project is open source and available at <a href="https://github.com/JastinXyz/cvmaker" target="_blank" className="underline">GitHub</a>.</p>
+            <p>The project source code is available at <a href="https://github.com/JastinXyz/cvmaker" target="_blank" className="underline">GitHub</a>.</p>
             <p>&copy; {new Date().getFullYear()} <a href="https://jstnlt.my.id" target="_blank" className="underline">jstnlt</a>.</p>
         </div>
     )
