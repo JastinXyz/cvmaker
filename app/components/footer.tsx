a00927d7870f387030334a6e9ee67b33ea34341a
JastinXyz <jastinlinggar@gmail.com>
2025-04-20 16:35:46 +0700

    feat: footer copyright

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return (
-        <div className="mt-4">
-            <p className="text-xs text-center">This project is open source and available at <a href="https://github.com/JastinXyz/cvmaker" className="underline">GitHub</a>.</p>
+        <div className="mt-4 text-xs text-center">
+            <p>This project is open source and available at <a href="https://github.com/JastinXyz/cvmaker" target="_blank" className="underline">GitHub</a>.</p>
+            <p>&copy; {new Date().getFullYear()} <a href="https://jstnlt.my.id" target="_blank" className="underline">jstnlt</a>.</p>
         </div>
     )
 }
